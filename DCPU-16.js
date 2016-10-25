90bd4503792275f715f789884d7be7e173482999
Charlotte Lily Fields <foxsan48@gmail.com>
2016-10-25 19:27:33 +1000

    added 0x08 operand

--- DCPU-16.js
+++ DCPU-16.js
@@ -1,11 +1,11 @@
-var MEM = ['0x00',//---------------- NOP
-           '0x01','0x01','0x05',//-- MOV B, 5
-           '0x01','0x02','0x01',//-- MOV C, 1
-           '0x02','0x00','0x02',//-- ADD A, C
-           '0x05','0x00','0x01',//-- IFE A, B
-           '0x04','0x07',//--------- JMP 1
-           '0x00',//---------------- NOP/\SKIPED
-           '0x00'];//--------------- NOP
+var MEM = [0x00,//---------------- NOP
+           0x01,0x01,0x05,//-- MOV B, 5
+           0x01,0x02,0x01,//-- MOV C, 1
+           0x02,0x00,0x02,//-- ADD A, C
+           0x05,0x00,0x01,//-- IFE A, B
+           0x04,0x07,//--------- JMP 1
+           0x00,//---------------- NOP/\SKIPED
+           0x00];//--------------- NOP
 var PC = 0;
 var registers = {
 	A : 0,
@@ -19,14 +19,15 @@ var registers = {
 };
 
 var operands = {
-	'0x00' : 'A',
-	'0x01' : 'B',
-	'0x02' : 'C',
-	'0x03' : 'X',
-	'0x04' : 'Y',
-	'0x05' : 'Z',
-	'0x06' : 'I',
-	'0x07' : 'J'
+	0x00 : 'A',
+	0x01 : 'B',
+	0x02 : 'C',
+	0x03 : 'X',
+	0x04 : 'Y',
+	0x05 : 'Z',
+	0x06 : 'I',
+	0x07 : 'J',
+	0x08 : MEM[registers['A']]
 }
 
 var fetch = function() {
@@ -36,28 +37,28 @@ var fetch = function() {
 }
 
 var opcodes = {
-	'0x00' : function nop(){}, //NOP
-	'0x01' : function mov(){/*MOV R, C*/
+	0x00 : function nop(){}, //NOP
+	0x01 : function mov(){/*MOV R, C*/
 		var R = operands[fetch()];
 		var c = fetch();
 		//console.log(parseInt(c,16));
 		registers[R] = parseInt(c,16);
 	},
-	'0x02' : function add(){ //-- ADD R, r
+	0x02 : function add(){ //-- ADD R, r
         var R = operands[fetch()];
         var r = operands[fetch()];
         registers[R] = registers[R] + registers[r];
     },
-    '0x03' : function sub(){ //-- SUB R, r
+    0x03 : function sub(){ //-- SUB R, r
         var R = operands[fetch()];
         var r = operands[fetch()];
         registers[R] = registers[R] - registers[r];
     },
-    '0x04' : function jmp(){ //-- JMP addr
+    0x04 : function jmp(){ //-- JMP addr
         var addr = fetch();
         PC = parseInt(addr,16);
     },
-    '0x05' : function ife(){ //-- IFE R, r
+    0x05 : function ife(){ //-- IFE R, r
         var R = registers[operands[fetch()]];
         var r = registers[operands[fetch()]];
         //PC = (R == r) and PC + 3 or PC
@@ -65,15 +66,19 @@ var opcodes = {
     }
 };
 
+var d2h = function(d) {
+	return '0x'+('00'+d.toString(16)).substr(-2).toUpperCase();
+}
 
 
 var FDX = function() {
-	console.log('PC IR   A B C D')
+	console.log('PC    IR   A    B    C')
 	while (PC < (MEM.length)){
 		var IR = fetch();
 		//PC += 1;
 		opcodes[IR]();
-		console.log(PC+'  '+IR+' '+registers['A']+' '+registers['B']+' '+registers['C']);
+		//('000000000000000' + i.toString(16)).substr(-16)
+		console.log(d2h(PC)+'  '+d2h(IR)+' '+d2h(registers['A'])+' '+d2h(registers['B'])+' '+d2h(registers['C']));
 	};
 };
 FDX();
